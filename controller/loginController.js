f164a30a92a9d8c0b3f7cf9ed20303f3f2484fa8
Samiran Samanta <samirans170@gmail.com>
2024-09-14 11:37:20 -0400

    Commit From Linux

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,6 +1,14 @@
 module.exports = async function loginuser(req, res) {
     try {
+        //Fetch the user data from req.body...
         const { username, email, password } = req.body;
+        //Check all required fields are provided or not..
+        if(!username || !email || !email){
+            res.status(400).send({
+                status: 'Failed',
+                message: "All fields are required."
+            });
+        }
         res.status(200).send({
             status: 'Success',
             data:{
