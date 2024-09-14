a6560b7536c6f847e26b97aeecea696f201786e2
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2024-09-14 21:35:11 +0530

    Main Test Commit

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,12 +1,21 @@
 module.exports = async function loginuser(req, res) {
     try {
+        //Fetch all user data from req.body...
         const { username, email, password } = req.body;
-        res.status(200).send({
-            status: 'Success',
-            data:{
-                username, email, password
-            }
-        })
+        // Check if all required fields are provided in the request body
+        if(!username || !email || !password){
+            res.status(400).send({
+                status: 'Failed',
+                message: "All fields are required."
+            });
+        }else{
+            res.status(200).send({
+                status: 'Success',
+                data:{
+                    username, email, password
+                }
+            })
+        }
     } catch (error) {
         res.status(500).send({
             status: 'Failed',
