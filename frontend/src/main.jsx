887b476de2c36423f5aec58c27e062c679b20e1d
Samiran2004 <samiran.samanta.dev@gmail.com>
2025-12-17 19:09:34 +0530

    update/Analytics[Frontend]<Samiran>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,17 @@
-// main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { AppProvider } from './context/appContext.jsx' // Add this import
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
 import { Analytics } from '@vercel/analytics/react';
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+import App from './App.jsx';
+import './index.css';
+import { AppProvider } from './context/appContext.jsx';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error('Missing Clerk Publishable Key');
 }
 
 createRoot(document.getElementById('root')).render(
@@ -19,9 +20,9 @@ createRoot(document.getElementById('root')).render(
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <AppProvider>
           <App />
-          <Analytics/>
         </AppProvider>
+        <Analytics />
       </ClerkProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
